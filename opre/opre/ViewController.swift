93a9463862119840743b8f2590bb6ec850994cc8
NotAlbertYee <32379648+NotAlbertYee@users.noreply.github.com>
2018-08-28 14:29:39 -1000

    changed things

---
+++ opre/opre/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  opre
+//
+//  Created by Albert Yee on 8/27/18.
+//  Copyright © 2018 Albert Yee. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var top: UITextField!
+    @IBOutlet weak var bottom: UITextField!
+    @IBAction func submit(_ sender: Any) {
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
